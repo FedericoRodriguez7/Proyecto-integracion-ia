98577649eae7ed881847544393663bb2da106820
federico rodriguez <tafedericoemanuel@gmail.com>
2025-05-16 12:36:13 -0300

    pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import { RouterProvider } from "react-router-dom"
 
 export const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">App</h1>
+    <RouterProvider router={ router }/>
   )
 }
---
+++ src/presentation/layouts/DashboardLayaout.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DashboardLayaout = () => {
+  return (
+    <div>DashboardLayaout</div>
+  )
+}
